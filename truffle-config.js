31ec44f1875e932861e38b7e4c39ff214883d672
vaibhavbunny <97833453+vaibhavbunny@users.noreply.github.com>
2023-10-25 00:28:47 +0530

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 5000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.19"
+    }
+  }
+}
